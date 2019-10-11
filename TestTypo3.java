f7bc3d272bd259785100e908caf6f7b0a91357f4
msukmoon <jaymoon9876@gmail.com>
2019-10-11 10:41:20 -0400

    Initial commit

---
+++ TestTypo3.java
@@ -0,0 +1,19 @@
+// Jay Moon
+
+import java.util.Scanner;
+
+public class TestTypo3 {
+
+	public static void main(String args[]) {
+
+		// Initialize scanner
+		Scanner sc = new Scanner(System.in);
+
+		// Ask user for input
+		System.out.print("Enter Address: ");
+
+		// Pass the input to getTypo3
+		Typo3 t3 = new Typo3();
+		t3.getTypo3(sc.nextLine());
+	}
+}
