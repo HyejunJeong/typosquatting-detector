1674f2f5f532c466c6aa22671f0a47c789003a12
Henry Crain <henry.crain@stonybrook.edu>
2019-10-23 19:12:48 -0400

    Added worker node dispatcher

--- WorkerNode.java
+++ WorkerNode.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Queue;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -8,31 +10,55 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class WorkerNode {
-	
-	private static String url = "www.yahoo.co";
-	
-	public static void main(String[] args) throws IOException{
+public class WorkerNode implements Runnable {
 
-		/** This path is a local path. */
-		String chromeDriverPath = "/usr/local/bin/chromedriver" ;
+	private String url;
+
+	// Notify when worker is finished
+	private final Queue<WorkerNode> queue;
+
+	public WorkerNode(Queue<WorkerNode> queue) {
+		this.queue = queue;
+	}
+
+	public void assignURL(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public void run() {
+		// This path is a local path.
+		String chromeDriverPath = "/usr/bin/chromedriver";
 	        
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors", "--silent");
+		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors", "--silent");
 		WebDriver driver = new ChromeDriver(options);
         
-		// get the url
-		driver.get("https://"+url);
+		// Get the url
+		driver.get("https://" + url);
         
 		// Get the source code of the page and save it to a file
-		File source = new File(url+"/"+url+".txt");
-		FileUtils.writeStringToFile(source, driver.getPageSource(), StandardCharsets.UTF_8.name());        
-        
+		try {
+			File source = new File("sites/" + url + "/" + url + ".html");
+			FileUtils.writeStringToFile(source, driver.getPageSource(), StandardCharsets.UTF_8.name());
+		} catch (IOException e) {
+			System.err.printf("Failed to get page source for %s%n", url);
+		}
+
 		// Take a screenshot of the current page
-		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(screenshot, new File(url+"/"+url+".png"));
-        
+		try {
+			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File("sites/" + url + "/" + url + ".png"));
+		} catch (IOException e) {
+			System.err.printf("Failed to get screenshot for %s%n", url);
+		}
+
+		synchronized (queue) {
+			queue.add(this);
+			queue.notify();
+		}
+
 		driver.quit();
 	}
 }
---
+++ WorkerNodeDispatcher.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class WorkerNodeDispatcher {
+
+    private static final int NUM_WORKERS = 10;
+    private final Queue<WorkerNode> workers;
+
+    public WorkerNodeDispatcher() {
+        workers = new LinkedList<>();
+
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            workers.add(new WorkerNode(workers));
+        }
+    }
+
+    public void dispatch(List<String> urls) throws InterruptedException {
+        for (String url: urls) {
+            WorkerNode worker;
+            synchronized (workers) {
+                if (workers.isEmpty()) {
+                    workers.wait();
+                }
+
+                worker = workers.remove();
+            }
+
+            worker.assignURL(url);
+            Thread thread = new Thread(worker);
+            thread.start();
+        }
+    }
+
+    // For testing
+    /*public static void main(String[] args) {
+        WorkerNodeDispatcher dispatcher = new WorkerNodeDispatcher();
+        String[] urlArr = { "www.google.com", "www.yahoo.com", "www.facebook.com", "www.wikipedia.org", "www.stonybrook.edu",
+                            "cs.stonybrook.edu", "www.reddit.com", "www.piazza.com", "www.linkedin.com", "www.bing.com",
+                            "www.paypal.com", "www.amazon.com", "www.amazon.co.uk", "www.netflix.com", "drive.google.com" };
+
+        List<String> urls = Arrays.asList(urlArr);
+        try {
+            dispatcher.dispatch(urls);
+        } catch (InterruptedException e) {
+            System.err.println("Dispatcher failed");
+        }
+    }*/
+}
