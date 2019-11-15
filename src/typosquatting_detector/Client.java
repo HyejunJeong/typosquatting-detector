eb76919c5010d6332549c1a29240392c0bb74518
nreimer <47394910+nreimer@users.noreply.github.com>
2019-11-15 11:11:33 -0500

    Updated Client.java

--- src/typosquatting_detector/Client.java
+++ src/typosquatting_detector/Client.java
@@ -4,16 +4,23 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Client implements Remote, ClientWork{
 	
 	public String communicate() {
-		return "hello from Client";
+		return "Hello from Service";
 	}
 	
 	public void go() throws RemoteException, NotBoundException, Exception {
 		Service tester = (Service) Naming.lookup("//localhost/Test");
-		System.out.println(tester.communicate());
+		Queue<String> list = new LinkedList<String>();
+		for(String s: tester.communicate().split(" ")){
+		      list.add(s);
+		}
+		System.out.println(list.toString());
+		//list here to give to worker nde dispatcher
 	}
 
 }
