ddf983fb78cc0f6e9ca58eca62504b3e3c7dcace
Jay Moon <jay@Jays-MacBook-Pro.local>
2019-11-15 10:56:13 -0500

    Added method contextDestroyed on ServletListener.java

--- src/typosquatting_detector/ServletListener.java
+++ src/typosquatting_detector/ServletListener.java
@@ -1,11 +1,18 @@
 package typosquatting_detector;
 
+import java.rmi.NoSuchObjectException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ServletListener implements ServletContextListener {
+	
+	Registry registry;
 	
 	@Override
 	public void contextInitialized(ServletContextEvent arg) {
@@ -13,11 +20,25 @@ public class ServletListener implements ServletContextListener {
 		System.out.println("hi from cl");
 		
 		try {
-			LocateRegistry.createRegistry(1099);
+			registry = LocateRegistry.createRegistry(1099);
 		} 
 		catch (RemoteException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg) {
+		// Close server
+		if (registry != null) {
+			try {
+				registry.unbind("Test");
+				UnicastRemoteObject.unexportObject(registry, true);
+			} 
+			catch (RemoteException | NotBoundException e) {
+				e.printStackTrace();
+			}
+		} 
 	}
 
 }
